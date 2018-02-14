790db4007095401f1cf3d9f38e453e36ba042716
InedoJohn <jrasch@inedo.com>
2018-02-14 17:21:42 -0500

    Fix AssemblyInfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,5 +1,16 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using Inedo.BuildMaster.Extensibility;
 
+[assembly: AssemblyTitle("InedoCoreLegacy")]
+[assembly: AssemblyDescription("Contains legacy actions for the BuildMaster InedoCore extension. The InedoCore extension must also be installed.")]
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyCompany("Inedo, LLC")]
 [assembly: AssemblyProduct("BuildMaster")]
-[assembly: RequiredBuildMasterVersion("5.0.0")]
+[assembly: AssemblyCopyright("Copyright © 2008 - 2016")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0")]
+[assembly: CLSCompliant(false)]
+[assembly: RequiredBuildMasterVersion("6.0.0")]
